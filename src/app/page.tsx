874e6c303733e397c24e507c1358feca024127b6
Daniel Deak <daniel.deak@postcodelottery.co.uk>
2025-10-24 14:44:52 +0100

    Fixing error type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,25 @@ import { getStoryblokApi } from "@/lib/storyblok";
 import { StoryblokStory } from "@storyblok/react/rsc";
 
 const Home = async () => {
-    try{
-  const { data } = await fetchData();
-  console.log('dat', data)
-  return (
-   <div>
-     <head>
-       <title>Create Next App</title>
-       <link rel="icon" href="/favicon.ico" />
-     </head>
-     <StoryblokStory story={data.story} />
-   </div>
- );
-    } catch (e) {
-        return <p> Something went wrong: {e.message}</p>
-        
-    }
-
-}
+  try {
+    const { data } = await fetchData();
+    return (
+      <div>
+        <head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <StoryblokStory story={data.story} />
+      </div>
+    );
+  } catch (e) {
+    return <p> Something went wrong: {(e as Error).message}</p>;
+  }
+};
 
 const fetchData = async () => {
   const storyblokApi = getStoryblokApi();
   return await storyblokApi.get(`cdn/stories/home`);
-}
+};
 
-export default Home
+export default Home;
