0e16bc588d62503f2efba98cba05194365d0b703
Daniel Deak <daniel.deak@postcodelottery.co.uk>
2025-10-25 15:29:02 +0100

    Added minimum title

--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -1,5 +1,10 @@
 const Title = () => {
-  return <h1 className="font-serif text-6xl z-10 text-primary">Daniel Deak</h1>;
+  return (
+    <div className="p-3 flex w-full items-center justify-center flex-col">
+      <h1 className="font-serif text-6xl z-10 text-primary">Daniel Deak</h1>
+      <h3 className="">Senior Frontend Developer</h3>
+    </div>
+  );
 };
 
 export default Title;
