3a3f4e77d450b51dd390514a92b62becef233b4c
Daniel Deak <daniel.deak@postcodelottery.co.uk>
2025-10-28 22:50:56 +0000

    Minor tweak

--- src/app/common/storyblok/useExcerpt.tsx
+++ src/app/common/storyblok/useExcerpt.tsx
@@ -3,6 +3,8 @@ import { renderRichText } from "@storyblok/react";
 // @ts-expect-error
 import type { StoryblokRichtext } from "@/app/types/storyblok.d.ts";
 
+const RICH_TEXT_MAX = 50;
+
 const useExcerpt = (richText: StoryblokRichtext) => {
   const html = renderRichText(richText);
   if (!html) return "";
@@ -11,7 +13,9 @@ const useExcerpt = (richText: StoryblokRichtext) => {
   tempDiv.innerHTML = html;
   const plainText = tempDiv.textContent || tempDiv.innerText || "";
 
-  return plainText.length > 50 ? plainText.substring(0, 10) + "..." : plainText;
+  return plainText.length > RICH_TEXT_MAX
+    ? plainText.substring(0, RICH_TEXT_MAX) + "..."
+    : plainText;
 };
 
 export default useExcerpt;
